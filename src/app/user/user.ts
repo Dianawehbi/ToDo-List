9f88bb2abfe5e7c6c66af1ec41178e33f87adfa6
Diana Wehbe <dianawehbiii@gmail.com>
2026-07-16 14:30:41 +0300

    update user compoenent

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,23 +1,45 @@
-import { Component, computed, EventEmitter, Input, input, output, Output, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  output,
+  Output,
+  signal,
+} from '@angular/core';
+import { User } from '../models/user.mode';
+import { CardComponent } from '../shared/card/card';
+
+// type aliases
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class UserComponent {
   // @Input({required: true}) id! : string;
   // @Output() select = new EventEmitter<string>();
-  
-  id = input.required<string>();
-  name = input.required<string>();
 
-  avatar = input.required<string>();
+  // id = input.required<string>();
+  // name = input.required<string | undefined>();
+
+  // avatar = input.required<string>();
+
+  user = input.required<User>();
+
+  selected = input.required<boolean>();
 
   select = output<string>();
 
-  imagePath = computed(()=> 'assets/users/'+ this.avatar());
+  imagePath = computed(() => 'assets/users/' + this.user().avatar);
   // get imagePath(){
   //   // we access this in html as property imagePath ✔️
   //   // not as method , imagePath() ❌
@@ -26,13 +48,7 @@ export class UserComponent {
 
   randomNumber = signal(1);
 
-  onSelectUser(){
-    this.select.emit(this.id());
+  onSelectUser() {
+    this.select.emit(this.user().id);
   }
-  // user: UserModel = input.required();
-
-  // onClick(){
-  //   // should be also an input from outside ,
-  //   // on clcik display the data of the list componenet based on the selected user
-  // }
 }
